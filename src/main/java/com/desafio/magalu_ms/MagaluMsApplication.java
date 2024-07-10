2751ab3a95edaa33b447148a4a99a6ee67bc42fe
giovanni.giovanelli <giovanni.giovanelli@hypeone.com>
2024-07-10 10:43:58 -0300

    primeiro commit, inserido Readme

---
+++ src/main/java/com/desafio/magalu_ms/MagaluMsApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.magalu_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluMsApplication.class, args);
+	}
+
+}
